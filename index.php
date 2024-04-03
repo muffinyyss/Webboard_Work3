3dc0fe22054f324f7469555dacdf3006ebb82d11
Muffinyyss <thitima.dengmee@gmail.com>
2024-04-03 23:22:54 +0700

    Add some code but its wrong and not work
    Add some code but its wrong and not work

--- index.php
+++ index.php
@@ -39,28 +39,46 @@
         <?php include "nav.php" ?>
 
     <div class="mt-3 d-flex justify-content-between">
-        <?php if(isset($_SESSION['id'])){ ?>
+        <!-- <?php if(isset($_SESSION['id'])){ ?> -->
         <div>
-             <label>หมวดหมู่</label>
+            <label>หมวดหมู่</label>
             <span class="dropdown">
-            <button class="btn btn-light btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            --ทั้งหมด--
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="Button2">
-                <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
                 <?php
-                    $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
-                    $sql = "SELECT * FROM category";
-                    foreach($conn->query($sql) as $row){
-                        echo "<li><a class=dropdown-item href=#>$row[name]</a></li>";
-                    }
-                    $conn = null;
-
-                    
+                    if(isset($_GET['cat_id']) && !empty($_GET['cat_id'])){
+                        $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
+                        $sql = "SELECT * FROM category as c
+                                WHERE c.id = {$_GET['cat_id']}";
+                        foreach($conn->query($sql) as $row){
                 ?>
+                        <button class="btn btn-light btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <?=$row['name']?>
+                        </button>
 
+                <?php
+                    }
+                    }else{    
+                ?>
+                    <button class="btn btn-light btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        --ทั้งหมด--
+                    </button>
+                <?php
+                    }
+                ?>
                 
-            </ul>
+                <ul class="dropdown-menu" aria-labelledby="Button2">
+                    <li><a class="dropdown-item" href="index.php">ทั้งหมด</a></li>
+                    <?php
+                        $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
+                        $sql = "SELECT * FROM category";
+                        foreach($conn->query($sql) as $row){
+                            echo "<li><a class='dropdown-item' href='index.php?cat_id={$row['id']}'>{$row['name']}</a></li>";
+                        }
+                        $conn = null;
+                    
+                    ?>
+
+                </ul>
+                
             </span>
         </div>
        
@@ -73,30 +91,51 @@
     <table class="table table-striped mt-4">
     <?php
         $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
+
         $sql = "SELECT t3.name, t1.title, t1.id, t2.login, t1.post_date FROM post as t1
         INNER JOIN user as t2 ON (t1.user_id = t2.id)
-        INNER JOIN category as t3 ON (t1.cat_id = t3.id) ORDER BY t1.post_date DESC";
+        INNER JOIN category as t3 ON (t1.cat_id = t3.id)";
+
+        if(isset($_GET['cat_id']) && !empty($_GET['cat_id'])){
+            $category_id = $_GET['cat_id'];
+            $sql .= " WHERE t1.cat_id = $category_id";
+        }
+        $sql .= " ORDER BY t1.post_date DESC";
+
         $result = $conn->query($sql);
+
         while($row = $result->fetch()){
             echo "<tr><td class='d-flex justify-content-between'>
-            <div>[ $row[0] ] <a href=post.php?id=$row[2]
-            style=text-decoration:none> $row[1]</a><br> $row[3] - $row[4]</div>";
-            if(isset($_SESSION['id']) && $_SESSION['role'] == 'a'){
-                echo "<div class='me-2 align-self-center'>
-                    <a class='btn btn-warning btn-sm' onclick='return edit()'><i class='bi bi-pencil-fill'></i></a>
-                    <a href=delete.php?id=$row[2]
-                    class='btn btn-danger btn-sm' onclick='return myFunction()' ><i class='bi bi-trash'></i></a>
-                
-                </div>";
 
-            }
-            echo "</td></tr>";
+                <div [{$row['name']}]
+                    <a href='post.php?id={$row['id']}' style='text-decoration:none'> {$row['title']} </a>
+                    <br>
+                    {$row['login']} - {$row['post_date']} </div> ";
+
+                if(isset($_SESSION['id'])){
+                    echo "<span style='display: flex; justify-content: center;'>";
+
+                    if(isset($_SESSION['id']) && $_SESSION['role'] == 'a') {
+                        echo "<span class='me-2 mt-2'><a href=delete.php?id=$row[id] class='btn btn-danger btn-sm' onclick='return myFunction()'><i class='bi bi-trash3'></i></a></span>";
+                        if($_SESSION['user_id'] == $row['user_id']) {
+                            echo "<span class='me-2 mt-2'><a href=editpost.php?id=$row[id] class='btn btn-warning btn-sm'><i class='bi bi-pencil-square'></i></a></span></span>";
+                        }
+                    }
+
+                    else if(isset($_SESSION['id']) && $_SESSION['role'] != 'a') {
+                        if($_SESSION['user_id'] == $row['user_id']) {
+                            echo "<span class='me-2 mt-2'><a href=delete.php?id=$row[id] class='btn btn-warning btn-sm'><i class='bi bi-pencil-square'></i></a></span></span>
+                            <span class='me-2 mt-2'><a href=editpost.php?id=$row[id] class='btn btn-warning btn-sm'><i class='bi bi-pencil-square'></i></a></span></span>";
+                        }
+                    }
+                }
+                echo "</td> </tr>";
+
         }
         $conn = null;
     ?>
 
     </table>
-
     </div>
 </body>
 </html>
